d39427270b97ba6c9b70ed1ee98485ac2e226406
hypnoticpenguin <chankevin234@gmail.com>
2021-01-11 22:41:01 -0500

    updated Todo List with forms, state, css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  //react hook to call function setInputText from Form.js and place it in <Form />
+  const [inputText, setInputText] = useState("");
+  //react hook to call function setTodos and place it in <Form />
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h1>Kev's To-Do List</h1>
       </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 //dropdown menu
-const From = () => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
+  //here I will write JS code/function
+  //inputTextHandler for each to do
+  const inputTextHandler = (e) => {
+    //console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+  // for each click of submit
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+  };
+
   return (
     <form>
-      <input type="text" class="todo-input" />
-      <button class="todo-button" type="submit">
-        <i class="fas fa-plus-square"></i>
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
       </button>
-      <div class="select">
-        <select name="todos" class="filter-todo">
+      <div className="select">
+        <select name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
@@ -18,3 +33,5 @@ const From = () => {
     </form>
   );
 };
+
+export default Form;
